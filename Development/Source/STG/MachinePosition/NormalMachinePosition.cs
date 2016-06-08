5d78b58460dc4b600963703d3391e73faf7fdac6
bosssanovar <bosssanovar@yahoo.co.jp>
2016-06-08 13:31:35 +0900

    単体テストが通らない変更を差し戻し

--- Development/Source/STG/MachinePosition/NormalMachinePosition.cs
+++ Development/Source/STG/MachinePosition/NormalMachinePosition.cs
@@ -47,25 +47,25 @@ namespace STG.MachinePosition
         /// 次のX軸負方向位置を取得します。
         /// </summary>
         /// <returns></returns>
-        protected override Position GetLeftPosition() => new Position(this.Position.X - 11, this.Position.Y);
+        protected override Position GetLeftPosition() => new Position(this.Position.X - 1, this.Position.Y);
 
         /// <summary>
         /// 次のX軸正方向位置を取得します。
         /// </summary>
         /// <returns></returns>
-        protected override Position GetRightPosition() => new Position(this.Position.X + 11, this.Position.Y);
+        protected override Position GetRightPosition() => new Position(this.Position.X + 1, this.Position.Y);
 
         /// <summary>
         /// 次のY軸負方向位置を取得します。
         /// </summary>
         /// <returns></returns>
-        protected override Position GetUnderPosition() => new Position(this.Position.X, this.Position.Y - 11);
+        protected override Position GetUnderPosition() => new Position(this.Position.X, this.Position.Y - 1);
 
         /// <summary>
         /// 次のY軸正方向位置を取得します。
         /// </summary>
         /// <returns></returns>
-        protected override Position GetUpperPosition() => new Position(this.Position.X, this.Position.Y + 11);
+        protected override Position GetUpperPosition() => new Position(this.Position.X, this.Position.Y + 1);
         #endregion
     }
 }
