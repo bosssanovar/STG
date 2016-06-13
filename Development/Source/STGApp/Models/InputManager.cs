091a1e0ad66e30f59aa5af7fda1b617c7e93d411
bosssanovar <bosssanovar@yahoo.co.jp>
2016-06-13 16:12:00 +0900

    KeyUp/Downを対応
    #16

--- Development/Source/STGApp/Models/InputManager.cs
+++ Development/Source/STGApp/Models/InputManager.cs
@@ -64,35 +64,67 @@ namespace STGApp.Models
         #region メソッド
 
         /// <summary>
-        /// 自機を左に移動します。
+        /// 自機の左移動命令を登録します。
         /// </summary>
-        public void MoveToLeft()
+        public void AddLeftOrder()
         {
             _InputManager.AddOrder(STG.InputManager.Order.MoveLeft);
         }
 
         /// <summary>
-        /// 自機を右に移動します。
+        /// 自機の左移動命令を削除します。
         /// </summary>
-        public void MoveToRight()
+        public void RemoveLeftOrder()
+        {
+            _InputManager.RemoveOrder(STG.InputManager.Order.MoveLeft);
+        }
+
+        /// <summary>
+        /// 自機の右移動命令を登録します。
+        /// </summary>
+        public void AddRightOrder()
         {
             _InputManager.AddOrder(STG.InputManager.Order.MoveRight);
         }
 
         /// <summary>
-        /// 自機を上に移動します。
+        /// 自機の右移動命令を削除します。
         /// </summary>
-        public void MoveToUpper()
+        public void RemoveRightOrder()
+        {
+            _InputManager.RemoveOrder(STG.InputManager.Order.MoveRight);
+        }
+
+        /// <summary>
+        /// 自機の上移動命令を登録します。
+        /// </summary>
+        public void AddUpOrder()
         {
             _InputManager.AddOrder(STG.InputManager.Order.MoveUp);
         }
 
         /// <summary>
-        /// 自機を下に移動します。
+        /// 自機の上移動命令を削除します。
         /// </summary>
-        public void MoveToUnder()
+        public void RemoveUpOrder()
+        {
+            _InputManager.RemoveOrder(STG.InputManager.Order.MoveUp);
+        }
+
+        /// <summary>
+        /// 自機の下移動命令を登録します。
+        /// </summary>
+        public void AddDownOrder()
         {
             _InputManager.AddOrder(STG.InputManager.Order.MoveDown);
+        }
+
+        /// <summary>
+        /// 自機の下移動命令を削除します。
+        /// </summary>
+        public void RemoveDownOrder()
+        {
+            _InputManager.RemoveOrder(STG.InputManager.Order.MoveDown);
         }
 
 
